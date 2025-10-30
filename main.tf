76167d6b91ebaea3999c395d60082dd14f1d7934
jxilong <jxilong@cn.ibm.com>
2025-10-30 17:30:32 +0800

    v5

--- main.tf
+++ main.tf
@@ -65,13 +65,13 @@ resource "google_compute_instance" "spark_worker" {
     network    = google_compute_network.spark_vpc.id
     subnetwork = google_compute_subnetwork.spark_subnet.id
     access_config {}
-    # 关键：通过 metadata 传递 Master 节点的内网 IP（推荐）或公网 IP
-    metadata = {
-      master_ip = google_compute_instance.spark_master.network_interface.0.network_ip  # 内网 IP（集群内通信更稳定）
-      # 若内网不通，可替换为：google_compute_instance.spark_master.network_interface.0.access_config.0.nat_ip
-    }    
   }
 
+  # 关键：通过 metadata 传递 Master 节点的内网 IP（推荐）或公网 IP
+  metadata = {
+    master_ip = google_compute_instance.spark_master.network_interface.0.network_ip  # 内网 IP（集群内通信更稳定）
+    # 若内网不通，可替换为：google_compute_instance.spark_master.network_interface.0.access_config.0.nat_ip
+  }    
   metadata_startup_script = file("${path.module}/startup/install-spark-worker.sh")
 
   tags = ["spark-cluster"]
